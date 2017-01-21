41320e3d7ac955824401caef7208e322c9411d50
gemohandy <bennetteidsness@gmail.com>
2017-01-21 14:05:11 -0600

    Cannon Ball Corrections
    Attempts to improve the firing system for the cannon balls.

--- Monkeying Waves/Assets/Scripts/cannonMouseTrack.cs
+++ Monkeying Waves/Assets/Scripts/cannonMouseTrack.cs
@@ -12,6 +12,8 @@ public class cannonMouseTrack : MonoBehaviour {
 	float curRot = 0;
 	float cannonPitch = 0;
 	float launchMagnitude = 10;
+	float correctionConst = 2f;
+	public Rigidbody cannonball;
 	RaycastHit Hit;
 
 	// Use this for initialization
@@ -24,7 +26,7 @@ public class cannonMouseTrack : MonoBehaviour {
 		mousePosition = Input.mousePosition;
 		mousePosition.z = 2;
 		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
-		Physics.Raycast (Camera.main.transform.position, mousePosition - Camera.main.transform.position, out Hit);
+		Physics.Raycast (Camera.main.transform.position, mousePosition - Camera.main.transform.position, out Hit, Mathf.Infinity, 1<<4);
 		xDist = Hit.point.x - this.transform.localPosition.x;
 		zDist = Hit.point.z - this.transform.localPosition.z;
 		lineDist = Mathf.Min (Mathf.Sqrt (xDist * xDist + zDist * zDist), 98f);;
@@ -33,6 +35,13 @@ public class cannonMouseTrack : MonoBehaviour {
 		cannonHead[1].Rotate(new Vector3(0, Mathf.Rad2Deg * Mathf.Asin(lineDist/launchMagnitude/Physics.gravity.magnitude)/2-cannonPitch, 0));
 		cannonHead[2].Rotate(new Vector3(0, Mathf.Rad2Deg * Mathf.Asin(lineDist/launchMagnitude/Physics.gravity.magnitude)/2-cannonPitch, 0));
 		cannonPitch = Mathf.Rad2Deg * Mathf.Asin (lineDist / launchMagnitude / Physics.gravity.magnitude) / 2;
-		Debug.Log (this.transform.rotation.eulerAngles);
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log ("Test");
+			Rigidbody ballClone = Instantiate (cannonball, new Vector3(transform.position.x, 0, transform.position.z), transform.rotation);
+			ballClone.velocity = new Vector3 (-Mathf.Cos (Mathf.Deg2Rad * (360-cannonHead[1].rotation.eulerAngles.x)) * Mathf.Cos (Mathf.Deg2Rad * this.transform.rotation.eulerAngles.y) * launchMagnitude,
+				Mathf.Sin (Mathf.Deg2Rad * (360-cannonHead[1].rotation.eulerAngles.x)) * launchMagnitude* correctionConst,
+				Mathf.Cos (Mathf.Deg2Rad * (360-cannonHead[1].rotation.eulerAngles.x)) * Mathf.Sin (Mathf.Deg2Rad * this.transform.rotation.eulerAngles.y) * launchMagnitude)* correctionConst;
+		}
+		Debug.Log (cannonHead [1].rotation.eulerAngles);
 	}
 }
